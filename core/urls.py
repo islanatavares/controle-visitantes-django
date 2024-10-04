74383e7ba26f8916fff80fd7e70aa94d4f4da5cf
islanatavares <islana.tavares@gmail.com>
2024-10-04 17:38:52 -0300

    feat: atualização do projeto

---
+++ core/urls.py
@@ -0,0 +1,14 @@
+
+from django.contrib import admin
+from django.urls import path
+from dashboard.views import index
+from visitantes.views import registrar_visitante, informacoes_visitante
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', index ,name='index'),
+    path('login/',auth_views.LoginView.as_view(template_name = 'login.html'),name= 'login'),
+    path('registrar-visitante/', registrar_visitante, name='registrar_visitante'),
+    path('visitante/<int:pk>', informacoes_visitante, name='visitante'),
+]
---
+++ visitantes/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib import messages
+from  django.contrib.auth.decorators import login_required
+from visitantes.forms import VisitanteForm
+from visitantes.models import Visitante
+
+@login_required
+def registrar_visitante(request):
+    form = VisitanteForm()
+    
+    if request.method == 'POST':
+        form = VisitanteForm(request.POST)
+        
+        if form.is_valid():
+            visitante = form.save(commit=False)
+            
+            visitante.registrado_por = request.user.porteiro
+            visitante = form.save()
+            
+            messages.success(request,'Visitante registrado com sucesso')
+            return redirect('index')
+        
+    context ={
+        'nome_pagina': 'Registrar visitante',
+        'form': form
+    }
+    
+    return render(request,'registrar_visitante.html',context)
+
+@login_required
+def informacoes_visitante(request, pk):
+    visitante = get_object_or_404 (Visitante, pk=pk)
+    
+    context ={
+        'nome_pagina': 'Informações Visitante',
+        'visitante' : visitante,
+    }
+    
+    return render(request,'informacoes_visitante.html',context)
